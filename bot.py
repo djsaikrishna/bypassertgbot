a45521a5438b9e0da207bb3e5b91defaa8ed7a70
Kat <askfriends@live.com.pk>
2022-12-21 15:03:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,22 +12,22 @@ def return_bypass():
     res = get_tld(userLink, as_object=True)
  
     if res.domain not in ["gplinks","try2link","adf","bitly","ouo","shareus","shortly","tinyurl","thinfi","hypershort","sirigan","gtlinks","theforyou","linkvertise","shortest","pkin","tekcrypt","short2url","rocklinks","rocklinks","moneykamalo","easysky","indianshortner","crazyblog","tnvalue","shortingly","dulink","bindaaslinks","pdiskshortener","mdiskshortner","earnl","rewayatcafe","crazyblog","bitshorten","rocklink","droplink","earn4link","tnlink","ez4short","xpshort","vearnl","adrinolinks","techymozo","linkbnao","linksxyz","short-jambo","droplink","linkpays","pi-l","tnlink","open2get","anonfiles","antfiles","1fichier","gofile","hxfile","krakenfiles","mdisk","mediafire","pixeldrain","racaty","sendcm","sfile","solidfiles","sourceforge","uploadbaz","uploadee","uppit","userscloud","wetransfer","yandex","zippyshare","fembed","mp4upload","streamlare","streamsb","streamtape","appdrive","gdtot","hubdrive","sharerpw"]:
-        print("That url is not supported yet, come back later!")
+        return("That url is not supported yet, come back later!")
     elif (res.domain == "link-center"):
         bypassed_link = bypasser.bypass(userLink, name="linkvertise")
-        print("Output URL>>",bypassed_link)
+        return("Output URL>>",bypassed_link)
         print("Made with Love by KATPER")
     elif (res.domain == "sharer"):
-        print ("sharer")
+        return ("sharer")
         print("Made with Love by KATPER")
     elif (res.domain == "gdtot"):
         crypt = input('\nEnter crypt value with double quotes\nExample "OXZIcTlGEF5Z3dxcTdkQdThJnBmKZdc5Dc6DcGhErWVhM2g5bjY0NnhlWERzQT0%3D"\n')
         bypassed_link = PyBypass.bypass(userLink, gdtot_crypt=crypt)
-        print("Output URL>>",bypassed_link)
+        return("Output URL>>",bypassed_link)
         print("Made with Love by KATPER")
     else:
         bypassed_link = bypasser.bypass(userLink)
-        print("Output URL>>",bypassed_link)
+        return("Output URL>>",bypassed_link)
         print("Made with Love by KATPER")
         #again = input("Wanna bypass another?")
 
