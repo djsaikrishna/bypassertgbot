2181afb8af8fba33e7a3f16d4a924aba990c40c9
Kat <askfriends@live.com.pk>
2022-12-30 02:30:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@
     added bot start info display in logs ---------------- 24-12-2022
     added logging info for each task in logs ------------ 28-12-2022
     added more formatting ------------------------------- 28-12-2022
+    added status msg when link is being converted ------- 28-12-2022
 """
 from telegram.ext.updater import Updater
 from telegram.update import Update
@@ -25,6 +26,7 @@ from tld import get_tld
 import PyBypass as bypasser
 import PyBypass
 import os
+import sys
 import logging
 
 #Made with Love by KATPER
@@ -99,8 +101,12 @@ def bypass(update, context):
         else:
             msg = sendMessage(f"⫸ <b>Processing:</b> <code>{url}</code>", context.bot, update)
             logging.info(f"Processing: {url}")
-            bypassed_link = bypasser.bypass(url)
-            deleteMessage(context.bot, msg)
+            try:
+                bypassed_link = bypasser.bypass(url)
+                deleteMessage(context.bot, msg)
+            except:
+                update.message.reply_text("Something went wrong!")
+                logging.info("something went wrong!")
             update.message.reply_text(f"➖➖➖➖➖➖➖➖➖➖➖➖\n"
                             f" *✅ Ad Link Bypassed!*\n"
                             f"➖➖➖➖➖➖➖➖➖➖➖➖\n\n"
@@ -130,6 +136,101 @@ def bypass(update, context):
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hello, This is bypasser bot made by KATPER SAHAB")
     logging.info("/start command!")
+
+"""
+def zip(update: Update, context: CallbackContext):
+    update.message.reply_text("This is zip command")
+    logging.info("/zip command!")
+    ASK_FIRST = input("\nBoth of your source and destination is GD/TD??\n1. Yes, both are GD/TD\n2. Noo. Its some other cloud storage\nChoose either 1/2:")
+    if ASK_FIRST not in ["1", "2"]:
+        print("Wrong input 🤬. Exiting!!!")
+        sys.exit()
+        if ASK_FIRST == "1":
+            CLONE_METHOD = "gclone"
+            SAS = input("Do you have service accounts and want to use in gclone? 🤔\n1. Yes\n2. No\nChoose either Y/N:")
+                if SAS not in ["Y", "N"]:
+                    print("wrong input 🤬. Exiting!!!")
+                    sys.exit()
+                if SAS == "Y":
+                    text_file = open("clone.conf", "w")
+                    text_file.write("[gs]\ntype = drive\nscope = drive\nservice_account_file = accounts/1.json\nservice_account_file_path = accounts/")
+                    text_file.close()
+                    RCLONE_PATH = ("/bot/clone.conf")
+                else:
+                    RCLONE_PATH = "/bot/.config/rclone/rclone.conf"
+        else:
+            CLONE_METHOD = "rclone"
+            RCLONE_PATH = "/bot/.config/rclone/rclone.conf"
+                if RCLONE_PATH == "/bot/.config/rclone/rclone.conf":
+                    if not os.path.isfile("/bot/.config/rclone/rclone.conf"):
+                        print("rclone.conf not found at /bot/.config/rclone/rclone.conf.\nYou can't use rclone/gclone without rclone.conf\nExiting!!!")
+                        sys.exit()
+
+    ASK_SECOND = input("Want to split? 👀\n1. Yes\n2. No\nChoose either Y/N:")
+        if ASK_SECOND not in ["Y", "N"]:
+        print("Wrong input 🤬. Exiting!!!")
+        sys.exit()
+            if ASK_SECOND == "Y":
+                sValue = input("Enter split size, Ex 500m or 10g")
+        else:
+            print("Noice, Let's go to next step\n\n")
+
+    ASK_THIRD = input ("What kind of output compressed file you want? 🤔\n1. zip\n2. 7z\n3. rar\nChoose either 1/2/3:")
+        if ASK_THIRD not in ["1", "2", "3"]:
+            print("Wrong input 🤬. Exiting!!!")
+            sys.exit()
+
+  
+    #Taking inputs from users
+    DESIRED_FOLDER_NAME = input("Provide Folder name:\n\nWhat's this?\nIt is the folder name, which user will get after extraction.So...please select an appropriate name")
+    DESIRED_FILE_NAME = input("Provide desired name for the resultant compressed file (with file extension name):\n\nProvide appropriate file extention name according to options selected above, otherwise you will face errors")
+    N = "/bot/files/"+DESIRED_FOLDER_NAME
+
+        if CLONE_METHOD == "gclone":
+            if SAS == "Y":
+                SOURCE_ID = input("Provide source ID:")
+                DESTINATION_ID = input("Provide Destination ID:")
+                S = "gs:{"+SOURCE_ID+"}"
+                D = "gs:{"+DESTINATION_ID+"}"
+            elif SAS == "N":
+                SOURCE_REMOTE = input("Provide Source remote:")
+                SOURCE_ID = input("Provide source ID:")
+                DESTINATION_REMOTE = input("Provide Destination remote:")
+                DESTINATION_ID = input("Provide Destination ID:")
+                S = SOURCE_REMOTE+":{"+SOURCE_ID+"}"
+                D = DESTINATION_REMOTE+":{"+DESTINATION_ID+"}"
+            else:
+                SOURCE_REMOTE = input("Provide Source remote:")
+                SOURCE_PATH = input("Provide source path:")
+                DESTINATION_REMOTE = input("Provide Destination remote:")
+                DESTINATION_PATH = input("Provide Destination path:")
+                S = SOURCE_REMOTE+":"+SOURCE_PATH
+                D = DESTINATION_REMOTE+":"+DESTINATION_PATH
+  
+    #Actual Process
+    print("Creating required directories....")
+    os.system("mkdir /bot/files && mkdir /bot/zip && mkdir /bot/files/+\"{0}\"".format(DESIRED_FOLDER_NAME))
+    print("\n\nDownloading to local storage.......")
+    os.system(CLONE_METHOD+" --config="+RCLONE_PATH+" copy \"{0}\" \"{1}\" -P -q --stats=10s".format(S,N))
+    print("\n\nStarted the compression.....")
+    
+        if ASK_SECOND == "Y":
+            if ASK_THIRD in ["1","2"]:
+                os.system("cd /bot/files && 7z a -mx=1 -v{0} /bot/zip/\"{1}\" ./\"{2}\" ".format(sValue,DESIRED_FILE_NAME,DESIRED_FOLDER_NAME))
+            else:
+                os.system("cd /bot/files && rar a -m0 -v{0} /bot/zip/\"{1}\" ./\"{2}\" ".format(sValue,DESIRED_FILE_NAME,DESIRED_FOLDER_NAME))
+        else:
+            if ASK_THIRD in ["1","2"]:
+                os.system("cd /bot/files && 7z a -mx=1 /bot/zip/\"{0}\" ./\"{1}\" ".format(DESIRED_FILE_NAME,DESIRED_FOLDER_NAME))
+            else:
+                os.system("cd /bot/files && rar a -m0 /bot/zip/\"{0}\" ./\"{1}\" ".format(DESIRED_FILE_NAME,DESIRED_FOLDER_NAME))
+    
+    print("\n\nStarted uploading......")
+    os.system(CLONE_METHOD+" --config="+RCLONE_PATH+" move /bot/zip \"{0}\" -P -q --stats=10s".format(D))
+    print("\n\nCleaning local storage....")
+    os.system("rm -r /bot/files && rm -r /bot/zip")
+
+"""    
 
 def owner(update: Update, context: CallbackContext):
     update.message.reply_text("Owner of this bot is 💪 KATPER SAHAB")
@@ -166,6 +267,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('owner', owner))
     updater.dispatcher.add_handler(CommandHandler('help', help))
+    #updater.dispatcher.add_handler(CommandHandler('zip', zip))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
     updater.dispatcher.add_handler(MessageHandler(
     # Filters out unknown commands
